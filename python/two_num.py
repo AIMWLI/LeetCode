a5f12f75f0220b508a55eb35614ffe10f76a9e25
AIMWLI <jin17.song@gmail.com>
2019-06-15 15:05:29 +0800

    two_num

--- python/two_num.py
+++ python/two_num.py
@@ -7,16 +7,18 @@
 所以返回 [0, 1]
 '''
 
+
 class solution:
 
     def two_num(self, nums, target):
         dict = {}
-        for index, num in enumerate(nums):
+        for index, num in enumerate(nums):  # {(0, 2), (1, 7),(2, 11)}
             another_num = target - num
             if another_num in dict:
                 return [dict[another_num], index]
             dict[num] = index
         return None
+
 
 nums = [2, 3, 4, 7, 11, 15]
 target = 9
